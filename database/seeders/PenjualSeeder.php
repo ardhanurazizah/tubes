91e7787cc7abc9dce4ff540de71475c7a6f0b0ca
rizka30 <2041720095@student.polinema.ac.id>
2022-06-01 19:27:06 +0700

    seeder database

--- database/seeders/PenjualSeeder.php
+++ database/seeders/PenjualSeeder.php
@@ -16,6 +16,15 @@ class PenjualSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $penjual = [
+            [
+                'id' => 1,
+                'id_users' => 1,
+                'nama' => 'Ardha Nur Azizah',
+                'alamat' =>'Jln. Basuki Rahmad no 03 Kab. Kediri',
+                'no' => '082559872333',
+            ],
+        ];
+        DB::table('penjual')->insert($penjual);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,10 +16,38 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Ardha Nur Azizah',
-            'email' => 'naardha0@gmail.com',
-            'password' => Hash::make('123456'),
-        ]);
+        $users = [
+            [
+                'name' => 'Ardha Nur Azizah',
+                'email' => 'naardha0@gmail.com',
+                'password' => Hash::make('123456'),
+           ],
+            [
+                'name' => 'Ayu Puspita',
+                'email' => 'ayupita@gmail.com',
+                'password' => Hash::make('231765'),
+            ],
+            [
+                'name' => 'Annisa Trihapsari',
+                'email' => 'triannisa@gmail.com',
+                'password' => Hash::make('343555'),
+            ],
+            [
+                'name' => 'Anang Subagyo',
+                'email' => 'nangyo730@gmail.com',
+                'password' => Hash::make('733799'),
+            ],
+            [
+                'name' => 'Diandra Dwi',
+                'email' => 'diandwi@gmail.com',
+                'password' => Hash::make('437177'),
+            ],
+            [
+                'name' => 'Bayu Saputra',
+                'email' => 'putraby@gmail.com',
+                'password' => Hash::make('987654'),
+            ],
+        ];
+        DB::table('users')->insert($users);
     }
 }
